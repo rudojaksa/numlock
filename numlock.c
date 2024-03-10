b085a0d58f837f3f7d92647b177bd4ebd97cb4b8
Rudolf Jaksa <rudolf.jaksa@gmail.com>
2024-03-10 15:12:54 +0100

    0.2a

--- numlock.c
+++ numlock.c
@@ -95,7 +95,6 @@ void toggle_keypress(Display *d) {
 // ------------------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
-
   for(int i=1;i<argc;i++) {
     if(!strcmp(argv[i],"-h")) { printf("%s\n    %s\n\n",HELP,SIGN); exit(0); }
     if(!strcmp(argv[i],"-v"))  VERBOSE=1;
